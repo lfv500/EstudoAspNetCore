0fbfbdaeacd2490c09fdc6297c6a68befa63f1fb
Luis Fernando Veloso <lfv500@gmail.com>
2021-02-18 11:54:48 -0300

    1.15 EXERCÍCIOS alterado meio de passar dados para a view ( de ViewBag injetar a variavel diretamente na view ) Criado no PostController os metodos: Adiciona e Novo e criado um view para cada metodo. Adicionado um construtor no PostController.

--- Blog/Controllers/PostController.cs
+++ Blog/Controllers/PostController.cs
@@ -9,22 +9,42 @@ namespace Blog.Controllers
 {
     public class PostController : Controller
     {
+
+        private IList<Post> lista;
+
+        public PostController()
+        {
+            this.lista = new List<Post>() 
+            { 
+                new Post() { Titulo = "Harry Potter 1", Resumo = "Pedra Filosofal", Categoria = "Filme, Livro" },
+                new Post() { Titulo = "Cassino	Royale", Resumo = "007", Categoria = "Filme"},
+                new Post() { Titulo = "Monge e o Executivo", Resumo = "Romance sobre Liderança", Categoria = "Livro"},
+                new Post() { Titulo = "New York, New York", Resumo = "Sucesso de Frank Sinatra", Categoria = "Música"}
+            };
+        }
+
         public IActionResult Index()
         {
             var	listaDePosts	=	new	List<Post>()
             {
 				new	Post()	{ Titulo = "Harry Potter 1", Resumo = "Pedra Filosofal", Categoria = "Filme, Livro" },
-
 				new	Post()	{ Titulo = "Cassino Royale", Resumo = "007", Categoria = "Filme" },
-
 				new	Post()	{ Titulo = "Monge e o Executivo", Resumo = "Romance sobre Liderança", Categoria = "Livro" },
-
 				new	Post()	{ Titulo = "New York, New York", Resumo = "Sucesso de Frank Sinatra", Categoria = "Música"}
-			};
+			};			
 
-			ViewBag.Posts	=	listaDePosts;
+            return View(listaDePosts);
+        }
 
+        public IActionResult Novo()
+        {
             return View();
+        }
+
+        public IActionResult Adiciona ( Post post)
+        {
+            lista.Add(post);
+            return View("Index", lista);
         }
     }
 }
