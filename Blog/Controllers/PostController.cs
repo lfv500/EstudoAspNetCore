e6fe58370142bda7106b51741c9c59e2cf5acc00
Luis Fernando Veloso <lfv500@gmail.com>
2021-02-18 09:27:30 -0300

    Add project files.

---
+++ Blog/Controllers/PostController.cs
@@ -0,0 +1,30 @@
+﻿using Blog.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog.Controllers
+{
+    public class PostController : Controller
+    {
+        public IActionResult Index()
+        {
+            var	listaDePosts	=	new	List<Post>()
+            {
+				new	Post()	{ Titulo = "Harry Potter 1", Resumo = "Pedra Filosofal", Categoria = "Filme, Livro" },
+
+				new	Post()	{ Titulo = "Cassino Royale", Resumo = "007", Categoria = "Filme" },
+
+				new	Post()	{ Titulo = "Monge e o Executivo", Resumo = "Romance sobre Liderança", Categoria = "Livro" },
+
+				new	Post()	{ Titulo = "New York, New York", Resumo = "Sucesso de Frank Sinatra", Categoria = "Música"}
+			};
+
+			ViewBag.Posts	=	listaDePosts;
+
+            return View();
+        }
+    }
+}
