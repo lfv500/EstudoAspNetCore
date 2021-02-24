68959616f070c35c76bbe90f996bbbcb5016a403
Luis Fernando Veloso <lfv500@gmail.com>
2021-02-24 16:27:30 -0300

    3.8 EXERCÍCIOS

--- Blog/Controllers/PostController.cs
+++ Blog/Controllers/PostController.cs
@@ -1,4 +1,5 @@
-﻿using Blog.Models;
+﻿using Blog.DAO;
+using Blog.Models;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -25,15 +26,20 @@ namespace Blog.Controllers
 
         public IActionResult Index()
         {
-            var	listaDePosts	=	new	List<Post>()
-            {
-				new	Post()	{ Titulo = "Harry Potter 1", Resumo = "Pedra Filosofal", Categoria = "Filme, Livro" },
-				new	Post()	{ Titulo = "Cassino Royale", Resumo = "007", Categoria = "Filme" },
-				new	Post()	{ Titulo = "Monge e o Executivo", Resumo = "Romance sobre Liderança", Categoria = "Livro" },
-				new	Post()	{ Titulo = "New York, New York", Resumo = "Sucesso de Frank Sinatra", Categoria = "Música"}
-			};			
+            //         var	listaDePosts	=	new	List<Post>()
+            //         {
+            //	new	Post()	{ Titulo = "Harry Potter 1", Resumo = "Pedra Filosofal", Categoria = "Filme, Livro" },
+            //	new	Post()	{ Titulo = "Cassino Royale", Resumo = "007", Categoria = "Filme" },
+            //	new	Post()	{ Titulo = "Monge e o Executivo", Resumo = "Romance sobre Liderança", Categoria = "Livro" },
+            //	new	Post()	{ Titulo = "New York, New York", Resumo = "Sucesso de Frank Sinatra", Categoria = "Música"}
+            //};			
 
-            return View(listaDePosts);
+            //         return View(listaDePosts);
+
+            PostDAO dao = new PostDAO();
+            IList<Post> lista = dao.Lista();
+            return View(lista);
+
         }
 
         public IActionResult Novo()
---
+++ Blog/DAO/PostDAO.cs
@@ -0,0 +1,39 @@
+﻿using Blog.Infra;
+using Blog.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog.DAO
+{
+    public class PostDAO
+    {
+		public IList<Post> Lista()
+		{
+			var lista = new List<Post>();
+			using (SqlConnection cnx = ConnectionFactory.CriaConexaoAberta())
+			{
+				SqlCommand comando = cnx.CreateCommand();
+				comando.CommandText = "select * from Posts";
+				SqlDataReader leitor = comando.ExecuteReader();
+				while (leitor.Read())
+				{
+					Post post = new Post()
+					{
+						Id = Convert.ToInt32(leitor["id"]),
+						Titulo = Convert.ToString(leitor["titulo"]),
+						Resumo = Convert.ToString(leitor["resumo"]),
+						Categoria = Convert.ToString(leitor["categoria"])
+					};
+					lista.Add(post);
+				}
+			}
+			return lista;
+		}
+
+
+	}
+
+}
